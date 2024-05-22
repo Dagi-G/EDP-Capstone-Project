4817e9eb7a32fbd132df4e003064ce6589b2cca2
Joshua Hughes <jhughe13@travelers.com>
2024-05-22 15:20:30 -0400

    Changes res.send to res.json in server. add effectControl to useEffect in SockPage.jsx

--- react/src/components/SocksPage.jsx
+++ react/src/components/SocksPage.jsx
@@ -2,37 +2,36 @@ import Sock from './Sock'
 import { useState, useEffect } from 'react';
 
 const SocksPage=()=> {
+    const effectControl = 1;
     const [socks,setSocks] = useState([])
     useEffect( () => {
-        const fetchSocks= async ()=> {
+        const fetchSocks = async () => {
             try {
-
-            const response=await fetch(`${import.meta.env.SERVER_URL}/socks`)
-          if (!response.ok){
-            throw new Error('Socks data coundent match')
-          }
-          const json_response=await response.json()
-          setSocks(json_response)
+                //console.log(import.meta.env.VITE_SERVER_URL)
+                const response=await fetch(`${import.meta.env.VITE_SERVER_URL}/socks`)
+                if (!response.ok){
+                    throw new Error('Socks data coundent match')
+                }
+                const json_response = await response.json()
+                console.log(json_response)
+                setSocks(json_response)
             } catch(error){
-
                 console.error('error fatching Socks',error)
             }
         }
         fetchSocks()
-    })
+    }, [effectControl])
     return (
-      
-       <div>
-        <h1>All socks for sale </h1>
         <div>
-        {
-            socks.map((sock)=>(
-            <Sock key = {sock.id}    data = {sock}/>
-            ))
-        }
-        </div>
-         </div>
-       
+            <h1>All socks for sale </h1>
+            <div>
+            {
+                socks.map((sock)=>(
+                <Sock key = {sock.id}    data = {sock}/>
+                ))
+            }
+            </div>
+        </div>  
     )
 }
 export default SocksPage
